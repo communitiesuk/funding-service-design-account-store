1a84c8a353a4aa645421b1d017e8655b49a54f46
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-04-29 12:17:16 +0100

    :feature: Queries now working for GET.

---
+++ app.py
@@ -0,0 +1,15 @@
+import connexion
+from flask import Flask
+
+
+def create_app() -> Flask:
+    connexion_app = connexion.FlaskApp(__name__, specification_dir="openapi/")
+
+    flask_app = connexion_app.app
+
+    connexion_app.add_api("api.yml")
+
+    return flask_app
+
+
+app = create_app()
--- core/account.py
+++ core/account.py
@@ -1,6 +1,39 @@
-def get_account_by_email():
-    pass
+from core.dumb_data_store import dummmy_db_connection
+
+if True:
+
+    db_connection = dummmy_db_connection()
+
+else:
+
+    db_connection = "a real connection"
+
+
+def get_account_by_email(email_address=None, account_id=None):
+
+    if email_address is None and account_id is None:
+
+        return 404
+
+    if email_address and account_id:
+
+        print("both given")
+
+        return 200
+
+    if email_address:
+
+        print("email given")
+
+        return 200
+
+    if account_id:
+
+        print("account id given")
+
+        return 200
 
 
 def post_account_by_email():
-    pass
+
+    print("post account worked")
