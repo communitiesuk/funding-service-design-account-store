1d80c48ae00f7e3b387e2220b2d9e52c637a78ee
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-05-05 15:35:58 +0100

    :bulb: Function docstrings added.

--- core/account.py
+++ core/account.py
@@ -1,13 +1,31 @@
 """
 Contains the functions directly used by the openapi spec.
 """
+from typing import Tuple
+
 import shortuuid
 from core.data_operations.account_data import check_exists_then_get
 from core.data_operations.account_data import get_data_by_email
 from core.db.db_connection import db_connection
 
 
-def get_account(email_address=None, account_id=None):
+def get_account(
+    email_address: str = None, account_id: str = None
+) -> Tuple[dict, int]:
+    """get_account Given an email or account id, the corresponding
+    entry in the db is returned.
+
+    Args:
+        email_address (str, optional): An email address given as a string.
+        Defaults to None.
+        account_id (_type_, optional): An account_id given as a string.
+        Defaults to None.
+
+    Returns:
+        dict, int
+    """
+
+    # print(f"email : {email_address} + account_id {account_id}")
 
     if email_address is None and account_id is None:
 
@@ -17,12 +35,25 @@ def get_account(email_address=None, account_id=None):
 
         return check_exists_then_get(account_id)
 
-    if email_address and not account_id:
+    if email_address and account_id is None:
 
         return get_data_by_email(email_address)
 
 
-def post_account_by_email(email_address):
+def post_account_by_email(email_address: str) -> Tuple[dict, int]:
+    """post_account_by_email Given an email address creates a new account in the
+    db.
+
+    If an account with this email address already exists then a 409
+     is returned,
+    otherwise a 200 is returned.
+
+    Args:
+        email_address (str): An valid email given as a string.
+
+    Returns:
+        Returns a dictionary(json) along with a status code.
+    """
 
     if db_connection.exists(f"email_{email_address}"):
 
--- core/data_operations/account_data.py
+++ core/data_operations/account_data.py
@@ -1,8 +1,19 @@
+from typing import Tuple
+
 from connexion import NoContent
 from core.db.db_connection import db_connection
 
 
-def check_exists_then_get(key):
+def check_exists_then_get(key: str) -> Tuple[dict, int]:
+    """check_exists_then_get Checks that the key exists in the db
+     and returns a value if so.
+
+    Args:
+        key (str): A key we would like the query in our db.
+
+    Returns:
+        A tuple with content and a status code.
+    """
 
     if not db_connection.exists(key):
 
@@ -11,7 +22,15 @@ def check_exists_then_get(key):
     return db_connection.get(key)
 
 
-def get_data_by_email(email):
+def get_data_by_email(email: str) -> Tuple[dict, int]:
+    """get_data_by_email Allows you to fetch account by its email.
+
+    Args:
+        email (str): An email given a str.
+
+    Returns:
+        A tuple with content and a status code. s
+    """
 
     address_id = check_exists_then_get(f"email_{email}")
 
