45410e081135e2da6311fd2d9b424ee50dbecc83
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-07-20 13:30:08 +0100

    :green_check_mark: Added db mock

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,33 @@
 """
 Contains test configuration.
 """
+import os
+
+import flask_migrate
 import pytest
+from config import Config
 from core.app import create_app
+
+
+class SqliteTestDB:
+    @classmethod
+    def remove(cls):
+        flask_root = Config.FLASK_ROOT
+        db_file_name = Config.SQLITE_DB_NAME
+        db_file_path = os.path.join(flask_root, db_file_name)
+        if os.path.exists(db_file_path):
+            os.remove(db_file_path)
+
+    @classmethod
+    def create(cls):
+        cls.remove()
+        flask_migrate.upgrade()
 
 
 @pytest.fixture()
 def app():
-    app = create_app()
-    return app
+    with create_app().app_context() as app_context:
+        SqliteTestDB.create()
+        with app_context.app.test_client() as test_client:
+            yield test_client
+        SqliteTestDB.remove()
