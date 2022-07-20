7da3f0250c8bc484713912048e438d7dc79029d9
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-07-20 13:37:44 +0100

    :test_tube: switched to flask test client

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,3 +31,18 @@ def app():
         with app_context.app.test_client() as test_client:
             yield test_client
         SqliteTestDB.remove()
+
+
+@pytest.fixture(scope="function")
+def flask_test_client():
+    """
+    Creates the test client we will be using to test the responses
+    from our app, this is a test fixture.
+    :return: A flask test client.
+    """
+
+    with create_app().app_context() as app_context:
+        SqliteTestDB.create()
+        with app_context.app.test_client() as test_client:
+            yield test_client
+        SqliteTestDB.remove()
--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -9,7 +9,7 @@ from tests.helpers import post_email_and_return_data
 
 
 class TestAccounts:
-    def test_new_post(self):
+    def test_new_post(self, flask_test_client):
         """test_new_post Creates a random email address and posts it
          to the account store,then checks that the email address
         exists in the account store.
@@ -21,7 +21,7 @@ class TestAccounts:
         """
 
         status_code, response_data = post_email_and_return_data(
-            self.client, "test@delete_me.org"
+            flask_test_client, "test@delete_me.org"
         )
 
         assert status_code == 201
@@ -29,20 +29,20 @@ class TestAccounts:
         assert "applications" in response_data.keys()
         assert response_data["email_address"] == "test@delete_me.org"
 
-    def test_double_post_returns_409(self):
+    def test_double_post_returns_409(self, flask_test_client):
 
         params = {"email_address": "test2@delete_me.org"}
 
         root_url = request.root_url
         url = root_url + "accounts"
 
-        response1 = self.client.post(url, json=params)
-        response2 = self.client.post(url, json=params)
+        response1 = flask_test_client.post(url, json=params)
+        response2 = flask_test_client.post(url, json=params)
 
         assert response1.status_code == 201
         assert response2.status_code == 409
 
-    def test_get_methods_work(self):
+    def test_get_methods_work(self, flask_test_client):
         """
         GIVEN An instance of our API
         WHEN Several get requests of
@@ -52,17 +52,17 @@ class TestAccounts:
         """
 
         _, response_dict = post_email_and_return_data(
-            self.client, email_address="test3@delete_me.org"
+            flask_test_client, email_address="test3@delete_me.org"
         )
 
         email = response_dict["email_address"]
         account_id = response_dict["account_id"]
 
         email_response_data = get_and_return_data(
-            self.client, email_address=email
+            flask_test_client, email_address=email
         ).data
         account_response_data = get_and_return_data(
-            self.client, account_id=account_id
+            flask_test_client, account_id=account_id
         ).data
         email_dict = ast.literal_eval(email_response_data.decode("utf-8"))
         account_dict = ast.literal_eval(account_response_data.decode("utf-8"))
@@ -76,10 +76,10 @@ class TestAccounts:
             == "test3@delete_me.org"
         )
 
-    def test_get_to_non_existing_resource_returns_204(self):
+    def test_get_to_non_existing_resource_returns_204(self, flask_test_client):
 
         response = get_and_return_data(
-            self.client, email_address="dfgdfjg@sdjlkjsf.org"
+            flask_test_client, email_address="dfgdfjg@sdjlkjsf.org"
         )
 
         assert response.status_code == 404
