528e56a00a4002f3b62feb7335c1ff24f648b10f
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-07-26 14:31:36 +0100

    :wrench: Added special url to test config.

--- config/envs/test.py
+++ config/envs/test.py
@@ -9,3 +9,7 @@ from fsd_utils import configclass
 class TestConfig(Config):
 
     SECRET_KEY = environ.get("SECRET_KEY", "test")
+
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL").replace(
+        "postgres://", "postgresql://"
+    )
