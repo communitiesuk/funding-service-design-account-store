989139978878a65dca444bda9182249cb6298fe1
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-07-27 09:53:45 +0100

    :ambulance: Dev config now using sqlite

--- config/envs/dev.py
+++ config/envs/dev.py
@@ -1,15 +1,21 @@
 """Flask Dev Pipeline Environment Configuration."""
 import logging
+from os import environ
+from os import path
 
-from config.envs.default import DefaultConfig as Config
+from config.envs.default import DefaultConfig
 from fsd_utils import configclass
 
 
 @configclass
-class DevConfig(Config):
+class DevConfig(DefaultConfig):
     #  Application Config
     SECRET_KEY = "dev"
     SESSION_COOKIE_NAME = "session_cookie"
 
     # Logging
     FSD_LOG_LEVEL = logging.INFO
+    SQLALCHEMY_DATABASE_URI = environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + path.join(DefaultConfig.FLASK_ROOT, "sqlite.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
