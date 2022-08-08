c3f5457ecb3fa1a2b20b7f731fafc293bc83d0a7
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-08-08 23:51:30 +0100

    :bug: Missing postgres replace

--- config/envs/default.py
+++ config/envs/default.py
@@ -18,5 +18,7 @@ class DefaultConfig(object):
     FSD_LOG_LEVEL = logging.WARNING
 
     # Database
-    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL").replace(
+        "postgres://", "postgresql://"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
