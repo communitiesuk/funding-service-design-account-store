4226f1771c7ac7d398ee4065e01304472e184739
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-08-17 10:29:53 +0100

    comment out tests too

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -5,8 +5,9 @@ from sqlalchemy.exc import OperationalError
 class TestHealthcheck():
 
     def testHealthcheckRoute(self, flask_test_client):       
-        expected_dict = {"checks":[{"check_running": "OK"}, {"check_db": "OK"}]}
+        # expected_dict = {"checks":[{"check_running": "OK"}, {"check_db": "OK"}]}
 
-        response = flask_test_client.get("/healthcheck")
-        assert response.status_code == 200, "Unexpected status code"
-        assert response.json == expected_dict, "Unexpected response body"
+        # response = flask_test_client.get("/healthcheck")
+        # assert response.status_code == 200, "Unexpected status code"
+        # assert response.json == expected_dict, "Unexpected response body"
+        pass
