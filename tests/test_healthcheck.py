7b33fa657f5cdb6a15d7133d0331ef73af504d0c
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2022-09-20 14:17:41 +0100

    fsd_utils v1.0.0 and fixed test with new return string

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -5,7 +5,7 @@ from sqlalchemy.exc import OperationalError
 class TestHealthcheck():
 
     def testHealthcheckRoute(self, flask_test_client):       
-        expected_dict = {"checks":[{"check_running": "OK"}, {"check_db": "OK"}]}
+        expected_dict = {"checks":[{"check_flask_running": "OK"}, {"check_db": "OK"}]}
 
         response = flask_test_client.get("/healthcheck")
         assert response.status_code == 200, "Unexpected status code"
