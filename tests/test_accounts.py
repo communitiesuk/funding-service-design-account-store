b5b9b3b31c00ec352fd43f83f7cadff0cc479c73
harryyo <56394038+harryyo@users.noreply.github.com>
2022-11-28 17:59:42 +0000

    Move account PUT endpoint tests into new TestAccountsPut class in test_accounts.py and split and rename post and get tests into TestAccountsPost and TestAccountsGet classes

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -5,7 +5,7 @@ from tests.helpers import get_and_return_data
 from tests.helpers import post_email_and_return_data
 
 
-class TestAccounts:
+class TestAccountsPost:
     def test_new_post(self, flask_test_client):
         """test_new_post Creates a random email address and posts it
          to the account store,then checks that the email address
@@ -37,6 +37,8 @@ class TestAccounts:
         assert response1.status_code == 201
         assert response2.status_code == 409
 
+
+class TestAccountsGet:
     def test_get_methods_work(self, flask_test_client):
         """
         GIVEN An instance of our API
@@ -79,3 +81,95 @@ class TestAccounts:
         )
 
         assert response.status_code == 404
+
+
+class TestAccountsPut:
+
+    test_email_1 = "person1@example.com"
+    test_email_2 = "person2@example.com"
+    accounts_created = {}
+
+    def test_update_full_name_and_role(self, flask_test_client):
+        """
+        GIVEN The flask test client
+        WHEN we PUT to the /accounts/{account_id} endpoint
+        WITH a json payload of
+            {
+                "role":"LEAD_ASSESSOR",
+                "full_name": "Jane Doe",
+            }
+        THEN the account full_name is updated
+
+        """
+        params = {"email_address": self.test_email_1}
+        url = "/accounts"
+
+        response = flask_test_client.post(url, json=params)
+
+        assert response.status_code == 201
+        assert response.json["email_address"] == self.test_email_1
+        self.accounts_created.update(
+            {self.test_email_1: response.json["account_id"]}
+        )
+
+        new_roles = ["ASSESSOR"]
+        new_full_name = "Jane Doe"
+        params = {"roles": new_roles, "full_name": new_full_name}
+        url = "/accounts/" + self.accounts_created.get(self.test_email_1)
+
+        response = flask_test_client.put(url, json=params)
+
+        assert response.status_code == 201
+        assert "email_address" in response.json.keys()
+        assert "full_name" in response.json.keys()
+        assert "roles" in response.json.keys()
+        assert response.json["email_address"] == self.test_email_1
+        assert response.json["full_name"] == new_full_name
+        assert response.json["roles"] == new_roles
+
+    def test_update_role_only(self, flask_test_client):
+        """
+        GIVEN The flask test client
+        WHEN we PUT to the /accounts/{account_id} endpoint
+        WITH a json payload of {"role":"LEAD_ASSESSOR"}
+        THEN the account role is updated
+
+        """
+        params = {"email_address": self.test_email_2}
+        url = "/accounts"
+
+        response = flask_test_client.post(url, json=params)
+
+        assert response.status_code == 201
+        assert response.json["email_address"] == self.test_email_2
+        self.accounts_created.update(
+            {self.test_email_2: response.json["account_id"]}
+        )
+
+        new_roles = ["LEAD_ASSESSOR"]
+        params = {"roles": new_roles}
+        url = "/accounts/" + self.accounts_created.get(self.test_email_2)
+
+        response = flask_test_client.put(url, json=params)
+
+        assert response.status_code == 201
+        assert "email_address" in response.json.keys()
+        assert "roles" in response.json.keys()
+        assert response.json["email_address"] == self.test_email_2
+        assert response.json["roles"] == new_roles
+
+    def test_update_role_with_non_existent_role_fails(self, flask_test_client):
+        """
+        GIVEN The flask test client
+        WHEN we PUT to the /accounts/{account_id} endpoint
+        WITH a json payload of {"role":"BAD_ROLE"}
+        THEN tan error is returned
+
+        """
+        new_roles = ["BAD_ROLE"]
+        params = {"roles": new_roles}
+        url = "/accounts/" + self.accounts_created.get(self.test_email_1)
+
+        response = flask_test_client.put(url, json=params)
+
+        assert response.status_code == 401
