75d24f192d6f93ec678d7748bbc20b2337aa73bf
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-06-15 08:02:53 +0100

    fixing tests for running on github

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -1,6 +1,9 @@
 class TestHealthcheck:
     def testHealthcheckRoute(self, flask_test_client):
-        expected_dict = {"checks": [{"check_flask_running": "OK"}, {"check_db": "OK"}]}
+        expected_dict = {
+            "checks": [{"check_flask_running": "OK"}, {"check_db": "OK"}],
+            "version": "123123",
+        }
 
         response = flask_test_client.get("/healthcheck")
         assert response.status_code == 200, "Unexpected status code"
