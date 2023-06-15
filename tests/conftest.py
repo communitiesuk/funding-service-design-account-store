1316a4378556300f16d3c729d9d494fed499b8f7
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-06-15 07:55:10 +0100

    bumping to latest utils and fixing tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,40 +1,21 @@
 """
 Contains test configuration.
 """
+from uuid import uuid4
+
 import pytest
 from app import create_app
-from config import Config
-from db import db
-from flask_migrate import upgrade
-from sqlalchemy_utils.functions import create_database
-from sqlalchemy_utils.functions import database_exists
-from sqlalchemy_utils.functions import drop_database
+from db.models.account import Account
+from db.models.role import Role
 
 
-def prep_db(reuse_db=False):
-    """Provide the transactional fixtures with access to the database via a
-    Flask-SQLAlchemy database connection."""
-    no_db = not database_exists(Config.SQLALCHEMY_DATABASE_URI)
-    refresh_db = not reuse_db
-
-    if no_db:
-
-        create_database(Config.SQLALCHEMY_DATABASE_URI)
-
-    elif refresh_db:
-
-        drop_database(Config.SQLALCHEMY_DATABASE_URI)
-        create_database(Config.SQLALCHEMY_DATABASE_URI)
-
-    upgrade()
+pytest_plugins = ["fsd_test_utils.fixtures.db_fixtures"]
 
 
 @pytest.fixture(scope="session")
 def app():
     app = create_app()
-    with app.app_context():
-        prep_db()
-    return app
+    yield app
 
 
 @pytest.fixture(scope="function")
@@ -50,16 +31,51 @@ def flask_test_client():
             yield test_client
 
 
+test_user_1 = {
+    "email": "seeded_user_1@example.com",
+    "subject_id": "subject_id_1",
+    "account_id": uuid4(),
+    "roles": ["COMMENTER"],
+}
+test_user_2 = {
+    "email": "seeded_user_2@example.com",
+    "subject_id": "subject_id_2",
+    "account_id": uuid4(),
+    "roles": ["COMMENTER"],
+}
+test_user_to_update = {
+    "email": "seeded_user_x@example.com",
+    "subject_id": "subject_id_x",
+    "account_id": uuid4(),
+    "roles": ["COMMENTER"],
+}
+
+
+def create_user_with_roles(user_config, db):
+    new_account = Account(
+        email=user_config["email"],
+        azure_ad_subject_id=user_config["subject_id"],
+        id=user_config["account_id"],
+    )
+    db.session.add(new_account)
+    db.session.commit()
+
+    roles_to_add = []
+    for r in user_config["roles"]:
+        role = Role(account_id=user_config["account_id"], role=r)
+        roles_to_add.append(role)
+    db.session.add_all(roles_to_add)
+
+    db.session.commit()
+
+
 @pytest.fixture(scope="session")
-def _db(app):
-    """
-    Provide the transactional fixtures with access
-    to the database via a Flask-SQLAlchemy
-    database connection.
-    """
-    return db
-
-
-@pytest.fixture(autouse=True)
-def enable_transactional_tests(db_session):
-    pass
+def seed_test_data(request, app, clear_test_data, _db):
+    marker = request.node.get_closest_marker("user_config")
+    if not marker:
+        users_to_create = [test_user_1, test_user_2, test_user_to_update]
+    else:
+        users_to_create = marker.args[0]
+    for user in users_to_create:
+        create_user_with_roles(user, _db)
+    yield users_to_create
