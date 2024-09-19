61d3b30fb885cb04a409bea52efc981db4b2d4ca
srh-sloan <srh-sloan@users.noreply.github.com>
2024-09-19 16:02:29 +0100

    Fs 4647 (#235)
    * utils bump
    
    * revert utils change

--- config/envs/default.py
+++ config/envs/default.py
@@ -4,17 +4,17 @@ import logging
 from os import environ
 from pathlib import Path
 
+from fsd_utils import CommonConfig
 from fsd_utils import configclass
 
 
 @configclass
 class DefaultConfig(object):
     #  Application Config
-    SECRET_KEY = environ.get("SECRET_KEY")
-    SESSION_COOKIE_NAME = environ.get("SESSION_COOKIE_NAME")
+    FLASK_ENV = CommonConfig.FLASK_ENV
+    SECRET_KEY = CommonConfig.SECRET_KEY
+    SESSION_COOKIE_NAME = CommonConfig.SESSION_COOKIE_NAME
     FLASK_ROOT = str(Path(__file__).parent.parent.parent)
-    FLASK_ENV = environ.get("FLASK_ENV")
-
     # Logging
     FSD_LOG_LEVEL = logging.WARNING
 
--- config/envs/development.py
+++ config/envs/development.py
@@ -9,10 +9,6 @@ from config.envs.default import DefaultConfig as Config
 
 @configclass
 class DevelopmentConfig(Config):
-    #  Application Config
-    SECRET_KEY = "dev"  # pragma: allowlist secret
-    SESSION_COOKIE_NAME = "session_cookie"
-    FLASK_ENV = "development"
 
     # Logging
     FSD_LOG_LEVEL = logging.DEBUG
